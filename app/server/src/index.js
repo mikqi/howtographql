cff947c00c93f50967455aa843cb0ea1b5f1ea60
Muhammad Rivki <Muhammad.Rivki@live.com>
2019-09-06 23:32:19 +0700

    refactor with imported resolver files

--- app/server/src/index.js
+++ app/server/src/index.js
@@ -1,40 +1,16 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('./prisma-client')
 
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: (root, args, context, info) => context.prisma.links(),
-    link: (parent, args, context) =>
-      context.prisma.links({
-        where: {
-          url: args.url,
-          id: args.id
-        }
-      })
-  },
-  Mutation: {
-    post: (parent, args, context) => {
-      return context.prisma.createLink({
-        url: args.url,
-        description: args.description
-      })
-    },
-    deleteLink: (parent, args) => {
-      const deleteIndex = links.findIndex(link => link.id === args.id)
-      const tempLinks = links[deleteIndex]
-      links.splice(deleteIndex, 1)
-      return tempLinks
-    },
-    updateLink: (parent, args) => {
-      const updateIndex = links.findIndex(link => link.id === args.id)
-      links[updateIndex] = {
-        ...links[updateIndex],
-        ...args
-      }
-      return links[updateIndex]
-    }
-  }
+  Query,
+  Mutation,
+  Link,
+  User
 }
 
 const server = new GraphQLServer({
