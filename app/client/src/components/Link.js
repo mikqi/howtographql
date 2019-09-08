877e21d80bfc93842056b2a1230cf643c15dbbb4
Muhammad Rivki <Muhammad.Rivki@live.com>
2019-09-08 17:33:44 +0700

    add vote mutation

--- app/client/src/components/Link.js
+++ app/client/src/components/Link.js
@@ -1,13 +1,56 @@
 import React from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+import { timeDifferenceForDate } from '../utls'
+import { AUTH_TOKEN } from '../constants'
 
-function Link(props) {
+function Link({ link, index }) {
+  const authToken = localStorage.getItem(AUTH_TOKEN)
+
   return (
-    <div>
-      <div>
-        {props.link.description} ({props.link.url})
+    <div className="flex mt2 items-start">
+      <div className="flex items-center">
+        <span className="gray">{index + 1}.</span>
+        {authToken && (
+          <Mutation variables={{ linkId: link.id }} mutation={VOTE_MUTATION}>
+            {voteMutation => (
+              <div className="ml1 gray f11" onClick={voteMutation}>
+                ▲
+              </div>
+            )}
+          </Mutation>
+        )}
+      </div>
+      <div className="ml1">
+        <div>
+          {link.description} ({link.url})
+        </div>
+        <div className="f6 lh-copy gray">
+          {link.votes.length} votes | by {link.postedBy ? link.postedBy.name : 'Unknown'}{' '}
+          {timeDifferenceForDate(link.createdAt)}
+        </div>
       </div>
     </div>
   )
 }
+
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      user {
+        id
+      }
+      link {
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Link
--- app/client/src/components/LinkList.js
+++ app/client/src/components/LinkList.js
@@ -14,8 +14,8 @@ const LinkList = () => {
 
         return (
           <div>
-            {linksToRender.map(link => (
-              <Link key={link.id} link={link} />
+            {linksToRender.map((link, index) => (
+              <Link key={link.id} link={link} index={index} />
             ))}
           </div>
         )
@@ -31,6 +31,17 @@ const FEED_QUERY = gql`
         id
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+            name
+          }
+        }
       }
     }
   }
