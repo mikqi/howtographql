45dc4b01fd28aafc225c99c16481ec7c41921671
Muhammad Rivki <Muhammad.Rivki@live.com>
2019-09-08 17:07:36 +0700

    add login functional

--- app/client/src/components/Login.js
+++ app/client/src/components/Login.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
 import { AUTH_TOKEN } from '../constants'
 
-const Login = () => {
+const Login = ({ navigate }) => {
   const [state, setState] = useState({
     login: true,
     email: '',
@@ -9,7 +11,11 @@ const Login = () => {
     name: ''
   })
 
-  const confirm = async () => {}
+  const confirm = async data => {
+    const { token } = state.login ? data.login : data.signup
+    saveUserData(token)
+    navigate('/')
+  }
   const saveUserData = token => window.localStorage.setItem(AUTH_TOKEN, token)
   return (
     <div>
@@ -18,34 +24,58 @@ const Login = () => {
         {!state.login && (
           <input
             value={state.name}
-            onChange={e => setState({ name: e.target.value })}
+            onChange={e => setState({ ...state, name: e.target.value })}
             type="text"
             placeholder="Your name"
           />
         )}
         <input
           value={state.email}
-          onChange={e => setState({ email: e.target.value })}
+          onChange={e => setState({ ...state, email: e.target.value })}
           type="text"
           placeholder="Your email address"
         />
         <input
           value={state.password}
-          onChange={e => setState({ password: e.target.value })}
+          onChange={e => setState({ ...state, password: e.target.value })}
           type="password"
           placeholder="Choose a safe password"
         />
       </div>
       <div className="flex mt3">
-        <div className="pointer mr2 button" onClick={() => confirm()}>
-          {state.login ? 'login' : 'create account'}
-        </div>
-        <div className="pointer button" onClick={() => setState({ login: !state.login })}>
+        <Mutation
+          mutation={state.login ? LOGIN_MUTATION : SIGNUP_MUTATION}
+          variables={{ email: state.email, password: state.password, name: state.name }}
+          onCompleted={data => confirm(data)}
+        >
+          {mutation => (
+            <div className="pointer mr2 button" onClick={mutation}>
+              {state.login ? 'login' : 'create account'}
+            </div>
+          )}
+        </Mutation>
+        <div className="pointer button" onClick={() => setState({ ...state, login: !state.login })}>
           {state.login ? 'need to create an account?' : 'already have an account?'}
         </div>
       </div>
     </div>
   )
 }
+
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+    signup(email: $email, password: $password, name: $name) {
+      token
+    }
+  }
+`
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`
 
 export default Login
